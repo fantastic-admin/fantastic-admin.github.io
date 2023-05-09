ccb53b2c99f945ab8662820bbf2c7bb0ddb52793
Hooray Hu <304327508@qq.com>
2023-05-10 00:36:38 +0800

    deploy

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -297,46 +297,59 @@ export default {
       ]
     },
     outline: 'deep',
-    algolia: {
-      appId: '97F9CKAB42',
-      apiKey: '4f597e49203732d2255b001e4b8367cf',
-      indexName: 'fantastic-admin',
-      placeholder: '搜索文档',
-      translations: {
-        button: {
-          buttonText: '搜索文档',
-          buttonAriaLabel: '搜索文档'
-        },
-        modal: {
-          searchBox: {
-            resetButtonTitle: '清除查询条件',
-            resetButtonAriaLabel: '清除查询条件',
-            cancelButtonText: '取消',
-            cancelButtonAriaLabel: '取消'
-          },
-          startScreen: {
-            recentSearchesTitle: '搜索历史',
-            noRecentSearchesText: '没有搜索历史',
-            saveRecentSearchButtonTitle: '保存至搜索历史',
-            removeRecentSearchButtonTitle: '从搜索历史中移除',
-            favoriteSearchesTitle: '收藏',
-            removeFavoriteSearchButtonTitle: '从收藏中移除'
-          },
-          errorScreen: {
-            titleText: '无法获取结果',
-            helpText: '你可能需要检查你的网络连接'
-          },
-          footer: {
-            selectText: '选择',
-            navigateText: '切换',
-            closeText: '关闭',
-            searchByText: '搜索提供者'
-          },
-          noResultsScreen: {
+    // algolia: {
+    //   appId: '97F9CKAB42',
+    //   apiKey: '4f597e49203732d2255b001e4b8367cf',
+    //   indexName: 'fantastic-admin',
+    //   placeholder: '搜索文档',
+    //   translations: {
+    //     button: {
+    //       buttonText: '搜索文档',
+    //       buttonAriaLabel: '搜索文档'
+    //     },
+    //     modal: {
+    //       searchBox: {
+    //         resetButtonTitle: '清除查询条件',
+    //         resetButtonAriaLabel: '清除查询条件',
+    //         cancelButtonText: '取消',
+    //         cancelButtonAriaLabel: '取消'
+    //       },
+    //       startScreen: {
+    //         recentSearchesTitle: '搜索历史',
+    //         noRecentSearchesText: '没有搜索历史',
+    //         saveRecentSearchButtonTitle: '保存至搜索历史',
+    //         removeRecentSearchButtonTitle: '从搜索历史中移除',
+    //         favoriteSearchesTitle: '收藏',
+    //         removeFavoriteSearchButtonTitle: '从收藏中移除'
+    //       },
+    //       errorScreen: {
+    //         titleText: '无法获取结果',
+    //         helpText: '你可能需要检查你的网络连接'
+    //       },
+    //       footer: {
+    //         selectText: '选择',
+    //         navigateText: '切换',
+    //         closeText: '关闭',
+    //         searchByText: '搜索提供者'
+    //       },
+    //       noResultsScreen: {
+    //         noResultsText: '无法找到相关结果',
+    //         suggestedQueryText: '你可以尝试查询',
+    //         reportMissingResultsText: '你认为该查询应该有结果？',
+    //         reportMissingResultsLinkText: '点击反馈'
+    //       }
+    //     }
+    //   }
+    // },
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: { buttonText: '搜索文档', buttonAriaLabel: '搜索文档' },
+          modal: {
             noResultsText: '无法找到相关结果',
-            suggestedQueryText: '你可以尝试查询',
-            reportMissingResultsText: '你认为该查询应该有结果？',
-            reportMissingResultsLinkText: '点击反馈'
+            resetButtonTitle: '清除查询条件',
+            footer: { selectText: '选择', navigateText: '切换', closeText: '关闭' }
           }
         }
       }
