5ce7529d04f539e30d78cb336d486cb85993d9ab
Hooray Hu <304327508@qq.com>
2024-04-11 15:17:46 +0800

    deploy

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -28,6 +28,13 @@ _hmt.push(['_requirePlugin', 'UrlChangeTracker', {
   s.parentNode.insertBefore(hm, s);
 })();
     `],
+    ['script', {}, `
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "lutgvw7b1c");
+    `],
   ],
   themeConfig: {
     logo: '/logo.png',
